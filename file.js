4e5a16249e83e5ae0cbe8e45fddaedfe2225271c
DragonCode369 <dragoncode369@gmail.com>
2022-10-14 22:59:23 +0200

    ~Changed text

--- file.js
+++ file.js
@@ -56,7 +56,7 @@ function game(playerSelection) {
         });
 
         if (playerPoints > computerPoints) {
-            gameOutput.innerHTML = '<h1 class="final-output">Victory!</h1>\n A lesson that you can take from this game: Taking chances are woth it!\n Here you go, one hint: You can influence the AI Choice ;]]';
+            gameOutput.innerHTML = '<h1 class="final-output">Victory!</h1>\n Taking chances is woth it!\n Hint: You can influence the AI Choice ;]]';
         }
 
         else if(playerPoints < computerPoints) {
